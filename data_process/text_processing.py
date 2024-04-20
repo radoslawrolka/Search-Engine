f6d99ff20bfb23f707af240d6b557ce132e55528
Radosław Rolka <radoslaw.rolka@gmail.com>
2024-04-20 21:00:07 +0200

    text proc update

--- data_process/text_processing.py
+++ data_process/text_processing.py
@@ -1,17 +1,18 @@
 import os
 import re
+import json
 from nltk.corpus import stopwords
 from nltk.stem import porter
 
 
-def text_preprocessing():
+def text_preprocessing(config):
     counter = 0
     all_file_dict = {}
     word_in_diff_file = {}
     stemmer = porter.PorterStemmer()
-    for file in os.listdir('./wiki'):
+    for file in os.listdir('./data/articles'):
         local_dict = {}
-        with open(f'./wiki/{file}', 'r') as f:
+        with open(f'./data/articles/{file}', 'r') as f:
             text = f.read()
             text = text.lower()
             text = re.sub(r'[^a-z]', ' ', text)
@@ -21,31 +22,30 @@ def text_preprocessing():
             words = [word for word in words if word not in stopwords.words('english')]
             words = [stemmer.stem(word) for word in words]
             words = [word for word in words if len(word) >= 3]
-            for word in words:
+            
+            local_dict = {word: words.count(word) for word in set(words)}
+            for word, v in local_dict.items():
                 if word not in word_in_diff_file:
                     word_in_diff_file[word] = 1
-                    all_file_dict[word] = 1
+                    all_file_dict[word] = v
                 else:
+                    all_file_dict[word] += v
                     word_in_diff_file[word] += 1
-                    all_file_dict[word] += 1
-                if word not in local_dict:
-                    local_dict[word] = 1
-                else:
-                    local_dict[word] += 1
 
-        with open(f'./dicts/{file}', 'w') as f:
-            f.write(f'{len(words)}\n')
-            for key, value in local_dict.items():
-                f.write(f'{key} {value}\n')
+        with open(f'./data/dicts/{file}.json', 'w') as f:
+            f.write(json.dumps(local_dict))
 
         counter += 1
         print(f'Processed {counter}')
 
-    with open('./main_dict.txt', 'w') as f:
-        for key, value in all_file_dict.items():
-            f.write(f'{key} {value}\n')
+    sorted_dict = sorted(all_file_dict.items(), key=lambda x: x[1], reverse=True)
+    sorted_dict = sorted_dict[:config['BAG_OF_WORDS']]
+    sorted_dict = dict(sorted_dict)
+    with open('./data/main_dict.json', 'w') as f:
+        f.write(json.dumps(sorted_dict))
     
-    with open('./word_in_diff_file.txt', 'w') as f:
-        for key, value in word_in_diff_file.items():
-            f.write(f'{key} {value}\n')
+    word_in_diff_file = {k: word_in_diff_file[k] for k in sorted_dict.keys()}
+    
+    with open('./data/words_in_diff_file.json', 'w') as f:
+        f.write(json.dumps(word_in_diff_file))
 
