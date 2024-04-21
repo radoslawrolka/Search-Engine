0893e3b398c0e8ac229e5a565f8ea06983478ea3
Radosław Rolka <radoslaw.rolka@gmail.com>
2024-04-21 23:26:25 +0200

    remove comment

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,10 @@ with open('config.json', encoding='utf-8') as f:
     config = json.load(f)
 
 app.config.update(config)
-"""
 app.config['MATRIX'] = sparse.load_npz('./data/matrix.npz')
-with open('./data/list_of_articles.txt', 'r') as f:
+with open('./data/list_of_articles.txt', 'r', encoding='utf-8') as f:
     app.config['LIST_OF_ARTICLES'] = f.read().split('\n')
-with open('.data/list_of_words.txt', 'r') as f:
+with open('.data/list_of_words.txt', 'r', encoding='utf-8') as f:
     app.config['LIST_OF_WORDS'] = f.read().split('\n')
-"""
+
 from app import routes
