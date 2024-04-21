8001a811b97dae690fb7380292a40d1a558293dd
Radosław Rolka <radoslaw.rolka@gmail.com>
2024-04-21 23:25:28 +0200

    server api done

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
 import json
 from flask import Flask
+from scipy import sparse
 
 app = Flask(__name__)
-with open('config.json') as f:
+with open('config.json', encoding='utf-8') as f:
     config = json.load(f)
 
 app.config.update(config)
-
+"""
+app.config['MATRIX'] = sparse.load_npz('./data/matrix.npz')
+with open('./data/list_of_articles.txt', 'r') as f:
+    app.config['LIST_OF_ARTICLES'] = f.read().split('\n')
+with open('.data/list_of_words.txt', 'r') as f:
+    app.config['LIST_OF_WORDS'] = f.read().split('\n')
+"""
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,29 @@
 from app import app
-from flask import render_template, redirect, url_for, json
+from flask import json, request, jsonify
+import data_process.text_processing as text_processing
+import data_process.matrix_creation as matrix_creation
+import engine.cosine as cosine_search
+import engine.svd as svd_search
+import engine.query as query_processing
+import sys
+sys.path.append('..')
 
-"""
-cosine_query_search(query, matrix, list_of_articles, list_of_words)
-svd_lowrank_query_search(query, list_of_articles, list_of_words, k=5)
-"""
-
-@app.route('/cosine')
+@app.route('/cosine', methods=['GET'])
 def cosine():
-    
+    query_param = request.args.get('query', default = '', type = str)
+    results_num = request.args.get('results_num', default = 10, type = int)
+    processed_query = query_processing.process_query(query_param)
+    vector = query_processing.to_vector(processed_query, app.config['LIST_OF_WORDS'])
+    result = cosine_search.search(vector, app.config['MATRIX'], results_num)
+    return jsonify({'result': result}), 200
+
+@app.route('/svd', methods=['GET'])
+def svd():
+    query_param = request.args.get('query', default = '', type = str)
+    results_num = request.args.get('results_num', default = 10, type = int)
+    k_param = request.args.get('k', default = 1000, type = int)
+    U, s, V = app.config['SVD_DICT'][k_param]
+    processed_query = query_processing.process_query(query_param)
+    vector = query_processing.to_vector(processed_query, app.config['LIST_OF_WORDS'])
+    result = svd_search.search(vector, U, s, V, results_num)
+    return jsonify({'result': result}), 200
